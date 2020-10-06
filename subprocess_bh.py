64c2ac4463a152bc568dec291362cdb5c60bba87
AGS <kvnaakash01@gmail.com>
2020-10-06 09:43:20 +0530

    Update subprocess_bh.py

--- subprocess_bh.py
+++ subprocess_bh.py
@@ -1,3 +1,5 @@
+# Program to access bh from python using subprocess and execute the calculator
+# Since this create different Pids for we cannot assign vaiables as we do in bh, for that we need to do multi threading 
 from subprocess import Popen,STDOUT,PIPE
 while True:
         query = input('$ ')
@@ -5,3 +7,4 @@ while True:
         p = Popen(['bc','-q','-i'], stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
         result = p.communicate(query.encode(), timeout=1)
         print(result[0].decode().rstrip())
+
